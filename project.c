31d8f262ad4d60733fed32a9ef12d04d823a2c9e
Priyavart Rajain <47432873+PriyavartRajain@users.noreply.github.com>
2020-08-04 15:03:37 -0600

    Update project.c

--- project.c
+++ project.c
@@ -1,8 +1,7 @@
 /*Project part 1
 Submitted by - Priyavart Rajain
 The University of Calgary 
-UCID- 30074184
-Course - CPSC 355 */
+Course - CPSC 355 Spring 2020*/
 
 #include <stdio.h>
 #include <stdlib.h>
